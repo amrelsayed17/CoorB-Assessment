42b5ab219546589a917e3714510865c67a40dd76
Amr ElSayed <amrelsayed17@yahoo.com>
2025-09-19 15:39:54 +0300

    Initial Commit

---
+++ CoorB Assessment/CoorB_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoorB_AssessmentApp.swift
+//  CoorB Assessment
+//
+//  Created by Amr ElSayed on 19/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoorB_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
